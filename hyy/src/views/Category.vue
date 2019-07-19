83ff930b77803c6d8e42ab8ed789fc7e421b9b47
HuangYanYu <695914800@qq.com>
2019-07-19 11:47:58 +0800

    day5

--- hyy/src/views/Category.vue
+++ hyy/src/views/Category.vue
@@ -5,9 +5,16 @@
     </div>
     <div class="main">
       <van-sidebar v-model="activeKey">
-        <van-sidebar-item v-for="(item,index) in this.Catedata" :title="item.mallCategoryName" to="/user"/>
+        <van-sidebar-item v-for="(item,index) in this.Catedata" :title="item.mallCategoryName" :keys="item.goodsId"/>
       </van-sidebar>
-      <div>
+      <div width="290px">
+        <van-tabs v-model="active" @click="onClick(id)">
+          <van-tab v-for="(item) in this.Catedata[activeKey].bxMallSubDto" :title ='item.mallSubName' >
+            <div style="width:290px;height:auto;">
+              {{item.mallSubName}}
+            </div>
+          </van-tab>
+        </van-tabs>
         <router-view></router-view>
       </div>
     </div>
@@ -24,16 +31,30 @@
     data() {
       return {
         activeKey:0,
-        Catedata:{}
+        active: 0,
+        Catedata:{},
+        selectData:{}
       };
     },
-    methods: {},
+    methods: {
+      onClick(id){
+        this.$axios.req('api/classification?mallSubId=${id}')
+          .then((res)=>{
+            if(res){
+              this.selectData=res;
+              console.log(this.selectData);
+            }
+          }).catch((err)=>{
+          console.log(err);
+        })
+      }
+    },
     mounted() {
       this.$axios.req('api/recommend')
         .then((res)=>{
           if(res){
             this.Catedata=res.data.data.category;
-            console.log(this.Catedata);
+            console.log(this.Catedata[0].bxMallSubDto[0].mallSubName);
           }
         }).catch((err)=>{
         console.log(err);
