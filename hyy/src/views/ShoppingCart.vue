8ebb50446532ee07ce0212a1dd4e3c01c7887af7
HuangYanYu <695914800@qq.com>
2019-07-19 10:06:23 +0800

    day5

--- hyy/src/views/ShoppingCart.vue
+++ hyy/src/views/ShoppingCart.vue
@@ -27,6 +27,6 @@
     }
 </script>
 
-<style scoped lang="scss">
+<style scoped>
 
 </style>
--- hyy/src/views/User.vue
+++ hyy/src/views/User.vue
@@ -1,6 +1,51 @@
 <template>
-    <div>
-        User
+    <div class="container">
+        <div class="header">
+          <p>会员中心</p>
+        </div>
+      <div class = 'bg-container'>
+        <div class="bg-item">
+          <div class="tx">
+            <img src="../assets/tx.jpg">
+          </div>
+          <p>
+            欢迎你
+          </p>
+          <p style="font-size: 15px">
+            退出登录
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <van-grid :border="false" :column-num="5">
+          <van-grid-item
+            :key="value"
+            icon="balance-o"
+            text="待付款"
+          />
+          <van-grid-item
+            :key="value"
+            icon="send-gift-o"
+            text="待发货"
+          />
+          <van-grid-item
+            :key="value"
+            icon="logistics"
+            text="待收货"
+          />
+          <van-grid-item
+            :key="value"
+            icon="comment-o"
+            text="评价"
+          />
+          <van-grid-item
+            :key="value"
+            icon="points"
+            text="已完成"
+          />
+        </van-grid>
+      </div>
     </div>
 
 </template>
@@ -27,6 +72,52 @@
   };
 </script>
 
-<style scoped lang="scss">
+<style scoped>
+  .container{
+    background-color: #eeeeee;
+    height:1000px;
+  }
+  .header{
+    padding: 2px;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    margin:0 auto;
+    width:100%;
+    height: 35px;
+    text-align: center;
+  }
+  .header p{
+    font-size:15px;
+  }
+  .tx{
+    border-radius:50%
+  }
+  .bg-container{
+    display: -webkit-flex;
+    display: flex;
+    background-color: #e30c7b;
+    width:375px;
+    height:220px;
 
+    color: white;
+    font-size: 22px;
+  }
+  .bg-item{
+    margin: auto;
+  }
+  .bg-item p{
+    text-align: center;
+    font-size: 22px;
+  }
+  .tx{
+    width:70px;
+    height:70px;
+    border-radius: 50%;
+    overflow: hidden;
+  }
+  .tx img{
+    width:100%
+  }
 </style>
