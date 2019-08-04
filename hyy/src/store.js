154414e573268b6499547252023f77d8bceca6ed
hyy <695914800@qq.com>
2019-08-05 01:23:45 +0800

    ..

--- hyy/src/store.js
+++ hyy/src/store.js
@@ -9,11 +9,13 @@ export default new Vuex.Store({
     activeKey:0,//首页选择的分类 默认选择第一个
     str:'2c9f6c946016ea9b016016f79c8e0000',//分类id 默认选择第一个美国车厘子
     selectedSubId:"",//分类中点击的商品
-    shopCart:{},//购物车属性
     user:{//用户属性
       keeplogin:0,//登录状态
-      username:''//用户名
-    }//用户属性
+      username:'',//用户名
+      avatar:'',//用户头像
+    },//用户属性
+    address:{},//待修改的地址信息
+    addressId:'',//待修改的地址id
   },
   mutations: {
     ['set_num'] (state, data) {
@@ -33,6 +35,15 @@ export default new Vuex.Store({
     },
     ['set_loginname'](state,data){
       state.user.username = data
+    },
+    ['set_addressId'](state,data){
+      state.addressId = data
+    },
+    ['set_address'](state,data){
+      state.address = data
+    },
+    ['set_avatar'](state,data){
+      state.user.avatar = data;
     }
   },
   actions: {
